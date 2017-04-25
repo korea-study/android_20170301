697154bd69baa432477bc181defb22c3f5505afe
daehui.kim <daehui.kim@systrangroup.com>
2017-04-25 22:31:33 +0900

    Example draw paint in canvas

--- app/src/main/java/com/company/demo/ex6/Example6Activity.java
+++ app/src/main/java/com/company/demo/ex6/Example6Activity.java
@@ -29,17 +29,32 @@ public class Example6Activity extends AppCompatActivity {
 
 
     class CustomView extends View {
-        Paint paint;
+        Paint paint1;
+        Paint paint2;
+        Paint paint3;
+
+
         public CustomView(Context context) {
             super(context);
-            paint = new Paint();
-            paint.setColor(Color.RED);
+            paint1 = new Paint();
+            paint1.setColor(Color.RED);
+
+            paint2 = new Paint();
+            paint2.setStyle(Paint.Style.STROKE);
+            paint2.setStrokeWidth(3);
+            paint2.setColor(Color.GREEN);
+
+            paint3 = new Paint();
+            paint3.setColor(Color.BLUE);
+            paint3.setStyle(Paint.Style.FILL);
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
-            canvas.drawRect(100, 100, 200, 200, paint);
+            canvas.drawRect(100, 100, 200, 200, paint1);
+            canvas.drawCircle(50, 160, 40, paint2);
+            canvas.drawCircle(300, 400, 50, paint3);
         }
     }
 }
