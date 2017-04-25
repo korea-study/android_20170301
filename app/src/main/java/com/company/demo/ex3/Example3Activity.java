90e9c655637e17dacf1ff15fbcb08160922f3b3e
DaeHui.KIM <DaeHui.KIM@systrangroup.com>
2017-04-25 17:17:05 +0900

    test transfer message arguments

--- app/src/main/java/com/company/demo/ex3/Example3Activity.java
+++ app/src/main/java/com/company/demo/ex3/Example3Activity.java
@@ -122,6 +122,10 @@ public class Example3Activity extends AppCompatActivity {
                                     e.printStackTrace();
                                 }
                                 Message message = pHandler.obtainMessage();
+                                message.what = 1;
+                                message.arg1 = 2;
+                                message.obj = new Object();
+
                                 pHandler.sendMessage(message);
                             }
                         }
@@ -133,11 +137,13 @@ public class Example3Activity extends AppCompatActivity {
     };
 
 
-    public class ProgressHandler extends Handler{
+    public class ProgressHandler extends Handler{ // 화면 작업에 특정 작업 적용시 컨트롤하기 위해서 Handler 상속 클래스 생성
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            Log.d("kkkk", "ProgressHandler");
+            Object object = msg.obj;
+            Log.d("kkkk", "ProgressHandler : " + msg.what + ", " + msg.arg1 + ", " + object.toString());
+
             threadProgressBar2.incrementProgressBy(10);
         }
     }
